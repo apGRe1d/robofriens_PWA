127f4ab97560aaf3873ff57518fd0faa2712a8d8
Andrei <you@example.com>
2023-01-18 16:42:25 +0300

    removed timeout from fetching

--- src/actions.js
+++ src/actions.js
@@ -13,16 +13,23 @@ export const requestRobots = (httpString) => (dispatch) => {
       return response.json();
     })
     .then(robots => {
-      return new Promise((res, rej) => {
-        setTimeout(() => {
-          if (!Object.keys(robots).length) {
-           return rej(new Error('ERROOOOOORR 404'));
-          }
-          dispatch({
-            type: FETCH_ROBOTS,
-            payload: robots
-          });
-        }, 2000);
+      // return new Promise((res, rej) => {
+      //   setTimeout(() => {
+      //     if (!Object.keys(robots).length) {
+      //      return rej(new Error('ERROOOOOORR 404'));
+      //     }
+      //     dispatch({
+      //       type: FETCH_ROBOTS,
+      //       payload: robots
+      //     });
+      //   }, 2000);
+      // });
+      if (!Object.keys(robots).length) {
+       throw new Error('ERROOOOOORR 404');
+      }
+      dispatch({
+        type: FETCH_ROBOTS,
+        payload: robots
       });
     })
     .catch(err => {
