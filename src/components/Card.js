6d03e58d2b2d19c14b3a23a92c39f4562f96d9b6
Andrei <you@example.com>
2023-01-18 17:24:41 +0300

    fixed image size for wide screens

--- src/components/Card.js
+++ src/components/Card.js
@@ -20,9 +20,13 @@ import React from 'react';
 // }
 
 const Card = ({ name, email, id }) => {
+  let imageSide = '200';
+  if (screen.width > 1920) {
+    imageSide = '400';
+  }
   return (
     <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-      <img alt='robots' src={`https://robohash.org/${id}.avif?size=200x200`} width='200px' height='200px' />
+      <img alt='robots' src={`https://robohash.org/${id}.avif?size=${imageSide}x${imageSide}`} width={`${imageSide}px`} height={`${imageSide}px`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const Scroll = (props) => {
+  let height = '800px';
+  if (screen.height > 1080) {
+    height = '1000px';
+  }
   return (
-    <div style={{ overflow: 'scroll', border: '5px solid black', height: '800px'}}>
+    <div style={{ overflow: 'scroll', border: '5px solid black', height: height}}>
       {props.children}
     </div>
   );
